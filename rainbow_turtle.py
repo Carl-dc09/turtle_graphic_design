9df47d0f105774df519b509ee08567d66b2931a8
unknown <diamantecarl9@gmail.com>
2024-10-01 14:57:42 +0800

    more layers added to make it looks like endless running loop

--- rainbow_turtle.py
+++ rainbow_turtle.py
@@ -1,13 +1,16 @@
-#Import from turtle
+# Import from turtle
 from turtle import *
-
-#Import colorsys for color
+# Import color from colorsys
 import colorsys
 
+# Set the drawing speed
 speed(0)
+# Set the background color
 bgcolor("black")
+# Set the hue to determine the color
 hue_color = 0
-for outer_loop in range(16):
+
+for outer_loop in range(200):
     for inner_loop in range(18):
         c = colorsys.hsv_to_rgb(hue_color, 1, 1)
         color(c)
@@ -20,4 +23,3 @@ for outer_loop in range(16):
     circle(40, 24)
 done()
 
-rt
