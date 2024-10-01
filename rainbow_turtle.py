87b46204b58e9a040015b34860e0692b8570032b
unknown <diamantecarl9@gmail.com>
2024-10-01 14:29:41 +0800

    revised variable

--- rainbow_turtle.py
+++ rainbow_turtle.py
@@ -3,12 +3,12 @@ import colorsys
 
 speed(0)
 bgcolor("black")
-h = 0
+height = 0
 for i in range(16):
     for j in range(18):
-        c = colorsys.hsv_to_rgb(h, 1, 1)
+        c = colorsys.hsv_to_rgb(height, 1, 1)
         color(c)
-        h += 0.005
+        height += 0.005
         rt(90)
         circle(150 - j * 6, 90)
         lt(90)
