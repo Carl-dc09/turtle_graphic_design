b85c97a2c165c86b64b98a2c32d69834f3ed4998
unknown <diamantecarl9@gmail.com>
2024-10-01 17:42:51 +0800

    Set the background color

--- heart_turtle.py
+++ heart_turtle.py
@@ -15,3 +15,14 @@ def heart_b(single_parameter):
 
 # Set the drawing speed
 speed(0)
+
+# Set the background color
+bgcolor("white")
+
+for i in range(6000):
+    goto(heart_a(i)*20, heart_b(i)*20)
+    for j in range(5):
+        color("#f73487")
+    goto(0, 0)
+
+done()
