643a6b654380fd43fe70f03a96094421fcd9e8b6
unknown <diamantecarl9@gmail.com>
2024-10-01 17:38:47 +0800

    Set the coordinates.

--- heart_turtle.py
+++ heart_turtle.py
@@ -1,3 +1,14 @@
-
+# Import the turtle module
 from turtle import *
+# Import the math module
 import math
+
+# Calculate and return the y-coordinate of the heart shape
+def heart_a(single_parameter):
+    return 15*math.sin(single_parameter)**3
+# Calculate and return the x-coordinate of the heart shape
+def heart_b(single_parameter):
+    return 12*math.cos(single_parameter)-5*\
+           math.cos(2*single_parameter)-2*\
+           math.cos(3*single_parameter)-\
+           math.cos(4*single_parameter)
