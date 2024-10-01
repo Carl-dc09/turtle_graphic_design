83b79ba893668e7670e811bb2c9bb4c738361383
unknown <diamantecarl9@gmail.com>
2024-10-01 19:05:32 +0800

    Added a loop.

--- circle_turtle.py
+++ circle_turtle.py
@@ -14,3 +14,13 @@ turtle_.speed(500)
 # Set a value for hue
 hue_increment = 70
 hue_value = 0
+
+# Create a loop
+for iteration_loop in range (720):
+    # Set the color
+    saturated_color = colorsys.hsv_to_rgb(hue_value, 1, 0.8)
+    hue_value += 1/hue_increment
+    turtle_.color(saturated_color)
+    # Set the position
+    turtle_.left(1)
+    turtle_.fd(1)
