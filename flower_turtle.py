fc2d0e8fb2906689445d594847082e2b09109258
unknown <diamantecarl9@gmail.com>
2025-01-27 18:34:18 +0800

    added some new features

--- flower_turtle.py
+++ flower_turtle.py
@@ -3,6 +3,8 @@ from turtle import *
 # Import color from colorsys
 import colorsys
 
+import time
+
 # Set the drawing speed
 speed(0)
 tracer(5)
@@ -12,6 +14,26 @@ hideturtle()
 bgcolor("black")
 # Set the hue for color
 hue_color = 0
+
+# Countdown to mark the start of drawing
+def start_countdown(seconds):
+     penup()
+     goto(0, 0)
+     color("white")
+     write("Starting in...", align="center", font=("Arial", 26))
+
+     for i in range(seconds, 0, -1):
+          clear()
+          write(f"Starting in {i}...", align="center", font=("Arial", 26))
+          update()
+          time.sleep(1)
+
+     clear()
+     write("Go!", align="center", font=("Arial", 24))
+     update()
+     time.sleep(1)
+     clear()
+     update()
 
 # To draw a specific pattern
 def draw_pattern(hue_color):
@@ -33,15 +55,22 @@ def draw_pattern(hue_color):
                right(180)
          circle(40, 24)
 
-# Set a multiple turtle's in a rows and columns
-for rows in range(2):
-     for columns in range(3):
-          penup()
-          goto(-500 + columns * 500, 300 - rows * 500)
-          pendown()
+def start_drawing():
+     start_countdown(5)
 
-          draw_pattern(hue_color)
-          hue_color += 0.1 
+     global hue_color
+
+     # Set a multiple turtle's in a rows and columns
+     for rows in range(2):
+          for columns in range(3):
+               penup()
+               goto(-500 + columns * 500, 300 - rows * 500)
+               pendown()
+
+               draw_pattern(hue_color)
+               hue_color += 0.1
+
+start_drawing() 
 
 # Finishes the drawing process          
 done()
